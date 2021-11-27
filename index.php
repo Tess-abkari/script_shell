70f98472bd00aa4b339dcffb1e760e7c377fb8a7
Tess abkari <tessabkari@gmail.com>
2021-11-27 13:38:12 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href="code.css" rel="stylesheet">
+
+    <title>form</title>
+  </head>
+  <body>
+    <h1>Connexion</h1>
+    <form >
+      <p>
+          <label>Login</label> : <input type="text" name="login" />
+          <input type="submit" value="Envoyer" name="submit"/>
+      </p>
+  </form>
+  </body>
+</html>
